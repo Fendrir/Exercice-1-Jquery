62f9c9cdf6491acadc898605a20fa5697597ee82
Julien <papao1@hotmail.fr>
2017-07-06 15:31:06 +0200

    exercice de terminé

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 
 console.log('hello world');  
+
+$("#addText").html('Hello world');
+
+// var addText = $('#addText');
+// addText.html('Hello world');
+
+// Exercie 2
+
+$('#remove').remove();
+
+
+// Exercice 3
+
+$('#changeColor').addClass('backgroundColor');
+
+// Exercice 4
+
+for (var i = 0; i < 4; i++) {
+
+$('#addChildren').append('<p>Yop</p>');
+
+
+}
